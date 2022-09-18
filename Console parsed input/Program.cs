2b12c6be495d98167f90f12b5dd1833c71c26a7b
Leonid Samoshenko <86494836+LeeDoor@users.noreply.github.com>
2022-09-18 19:19:17 +0300

    build, testcases

--- Console parsed input/Program.cs
+++ Console parsed input/Program.cs
@@ -10,9 +10,13 @@ namespace Console_parsed_input
     {
         public static void Main()
         {
-            Console.WriteLine("enter int: ");
+            Console.WriteLine("enter int, where 10 < int < 20: ");
             int a = ConsoleReader.ReadInt(n => n > 10 && n < 20);
             Console.WriteLine($"{a} value");
+
+            Console.WriteLine("enter string where 10 < string's length < 30: ");
+            string b = ConsoleReader.ReadString(n => n.Length > 10 && n.Length < 30);
+            Console.WriteLine($"{b} string with {b.Length} length");
         }
     }
 }
