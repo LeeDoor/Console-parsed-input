6a1ddc248de19c2fb61c243511df4750daaef00a
Leonid Samoshenko <86494836+LeeDoor@users.noreply.github.com>
2022-09-18 19:13:33 +0300

    init

---
+++ Console parsed input/ConsoleReader.cs
@@ -0,0 +1,59 @@
+﻿namespace Console_parsed_input
+{
+    public static class ConsoleReader
+    {
+        /// <summary>
+        /// ask the user until it gets a number in the console
+        /// </summary>
+        /// <returns>given number</returns>
+        public static int ReadInt()
+        {
+            string? inp;
+            int val = -1;
+
+            do inp = Console.ReadLine();
+            while (string.IsNullOrEmpty(inp) || !Int32.TryParse(inp, out val));
+
+            return val;
+        }
+
+        /// <summary>
+        /// ask the user until it gets a number suitable for the conditions in the console
+        /// </summary>
+        /// <param name="condition">condition for needed number</param>
+        /// <returns>returns number</returns>
+        public static int ReadInt(Predicate<int> condition)
+        {
+            string? inp;
+            int val = -1;
+
+            while (true)
+            {
+                inp = Console.ReadLine();
+
+                if (!string.IsNullOrEmpty(inp) && Int32.TryParse(inp, out val))
+                {
+                    if (!condition.Invoke(val))
+                        continue;
+
+                    return val;
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// ask user until it gets a string in the console
+        /// </summary>
+        /// <returns>returns given string</returns>
+        public static string ReadString()
+        {
+            string? inp;
+
+            do inp = Console.ReadLine();
+            while (string.IsNullOrEmpty(inp));
+
+            return inp;
+        }
+    }
+}
---
+++ Console parsed input/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_parsed_input
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("enter int: ");
+            int a = ConsoleReader.ReadInt(n => n > 10 && n < 20);
+            Console.WriteLine($"{a} value");
+        }
+    }
+}
